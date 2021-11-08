b9650de8ccbf5a187f7f0289c31e24bda401fd11
Guibert-S <77342727+Guibert-S@users.noreply.github.com>
2021-11-08 15:52:31 +0100

    Create Magasin.java

---
+++ Magasin.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; 
+
+public class Magasin {
+	private double depense;
+	private double revenu;
+	// nombre d'articles dans le magasin //
+	private Article [] articles = new Article[25];
+	static Scanner objet = new Scanner (System.in);
+	
+	// ajout d'article et saisie controle avex la boucle while //
+	public Article creationArticle() {
+		String nomArticle = null;
+		double prixAchat = 0;
+		double prixVente = 0;
+		
+		while (nomArticle==null | prixAchat ==0 | prixVente ==0) {
+			System.out.print("Nom de l'article :" );
+			nomArticle = objet.nextLine(); // String // 
+			System.out.print("Prix d'achat de l'article :" );
+			prixAchat= objet.nextDouble();
+			System.out.print("Prix  de vente de l'article :" );
+			prixVente = objet.nextDouble();
+		}
+		return new Article(nomArticle,prixAchat,prixVente);
+	}
+	// Ajouter l'article à la liste/array/dictionnaire //
+	public void ajouterArticle() {
+		for (int i=0;i<articles.length;i++);
+			if (articles[i]==null) {
+				articles[i] = creationArticle();	
+			}
+	}
+	
+}
