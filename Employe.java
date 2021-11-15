19dc9c23aafa80780a17f43e6bddb20dbb7f1f32
Nath149 <81323071+Nath149@users.noreply.github.com>
2021-11-15 17:33:18 +0100

    Create Employe.java

---
+++ Employe.java
@@ -0,0 +1,16 @@
+public abstract class Employe {
+	protected String nom;
+	protected String prenom;
+	protected String cv;
+	protected Poste emploi;
+	protected int salaire;
+	
+	
+	public void recevoirSalaire() {
+		
+	}
+	public void presentation() {
+		
+	}
+
+}
