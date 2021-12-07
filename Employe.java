afb87bc466b92d2ceeed47294b686b9a947e586d
Nath149 <81323071+Nath149@users.noreply.github.com>
2021-12-07 14:28:07 +0100

    Update Employe.java

--- Employe.java
+++ Employe.java
@@ -1,10 +1,20 @@
 public abstract class Employe {
 	protected String nom;
 	protected String prenom;
-	protected String cv;
 	protected Poste emploi;
 	protected int salaire;
 	
+	public Employe(String nom, String prenom, Poste emploi,int salaire) {
+		this.nom=nom;
+		this.prenom=prenom;
+		this.emploi=emploi;
+		this.salaire=salaire;
+		
+		
+	}
+	public String getNom() {
+		return this.nom;
+	}
 	
 	public void recevoirSalaire() {
 		
@@ -13,4 +23,7 @@ public abstract class Employe {
 		
 	}
 
+	public String getPrenom() {
+		return this.prenom;
+	}
 }
