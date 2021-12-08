8160b6b3a7909ad22ed67907b30c0588ce1d8cb3
Nath149 <81323071+Nath149@users.noreply.github.com>
2021-12-08 15:11:46 +0100

    Update Magasin.java

--- Magasin.java
+++ Magasin.java
@@ -1,58 +1,72 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Scanner;
 
-public class Magasin extends Client {
-	public Magasin(String nom, String prenom, int argent) {
-		super(nom, prenom, argent);
-		// TODO Auto-generated constructor stub
-	}
-
-	private double depense;
-	private double revenu;
+public class Magasin {
+	private String nom;
+	private static double revenu;
+	private static double depense;
 	// nombre d'articles dans le magasin //
-	static Map <Integer,Article> articles = new HashMap<>();// dictionnaire reliant les articles avec leur stock  // j'ai passe en static sinon ca marchait pas
-	private Map <Client,Integer> reduction = new HashMap<>();// dictionnaire reliant le client avec leur nombre d'achats 
+	private static Map<Article,Integer> articles = new HashMap<>();// dictionnaire reliant les articles avec leur stock 
+	private static Map <Client,Integer> reduction = new HashMap<>();// dictionnaire reliant le client avec leur nombre d'achats 
 	static Scanner objet = new Scanner (System.in);
+	private ArrayList <Employe> employes=new ArrayList <Employe>();// liste dynamique d'employe
+	public Magasin() {}
 	
 	// ajout d'article et saisie controle avec la boucle while //
+	
 	public static Article creationArticle() {
 		String nomArticle = null;
 		double prixAchat = 0;
 		double prixVente = 0;
-		int promo = 0 ;
-		int stock = 0 ; 
-		String description = null ;
-		while (nomArticle == null || description == null || prixAchat ==0 || prixVente ==0  || promo ==0 || stock ==0) {
+		
+		while (nomArticle == null || prixAchat ==0 || prixVente ==0) {
 			
 			System.out.print("Nom de l'article :\n" );
 			nomArticle = objet.nextLine(); // String // 
-			nomArticle =objet.nextLine();
-			System.out.print("Description de l'article :\n" );
-			description = objet.nextLine(); // String // 
-			 
+			
 			System.out.print("Prix d'achat de l'article :\n" );
 			prixAchat= objet.nextDouble();
 			
 			System.out.print("Prix  de vente de l'article  :\n" );
 			prixVente = objet.nextDouble();
+			objet.nextLine() ; // si on met qu'une fois nextline ca marche pas pas donc bon ... //
 			
-			System.out.print(" Article en promotion ?  :\n" );
-			promo = objet.nextInt();
-			
-			System.out.print(" Stock?  :\n" );
-			stock = objet.nextInt();
-			
-			
-
-			 
 		}
-		return new Article(nomArticle, description,  prixAchat,  prixVente,  promo,stock);
+		return new Article(nomArticle,prixAchat,prixVente);
 	}
 	
+	public Article traduction(String s) {
+		Iterator it=articles.entrySet().iterator();
+		while(it.hasNext()) {
+			Map.Entry m=(Map.Entry) it.next();
+			if(((Article) m.getKey()).getNom()==s) {
+				return (Article) m.getKey();
+			}
+		}
+		return null;
+		
+		
+		
+	}
+	public void ajoutEmploye(Employe e) {
+		employes.add(e);
+	}
+	public void afficherEmploye() {
+		Iterator<Employe> it =employes.iterator();
+		while(it.hasNext()) {// tant que it.hasnext est different de nul, on continue dans la boucle.
+			Employe a=it.next();
+			System.out.println("Nom:"+a.getNom()+" Prenom: "+a.getPrenom());
+		}
+	}
+	public void licenciement(Employe e) {
+		employes.remove(e); // qaund on tombe sur l'employe à supprumer, on le supprime de l'array.		
+	}
+	// Ajouter l'article à la liste/array/dictionnaire //
 	public void afficherArticle(Article a) {
-		System.out.println("Nom article: "+a.getNom()+"Prix d'achat: "+a.getPrixAchat()+"Prix de vente: "+a.getPrixVente()+"Descrption"+a.getDescription());
+		System.out.println("Nom article: "+a.getNom()+"Prix d'achat: "+a.getPrixAchat()+"Prix de vente: "+a.getPrixVente());
 	}
 	public static void ajouterArticle(Article a) {
 		System.out.println("Quelle est le stock de votre article ?");
@@ -60,6 +74,10 @@ public class Magasin extends Client {
 		articles.put(a, c);
 	}
 	
+	public void ticket(Article s) {
+		
+		
+	}
 	public void vendre(Client c,Article a) {
 		int d=reduction.get(c);
 		if(c.getArgent()<a.getPrixVente()) {
@@ -81,52 +99,38 @@ public class Magasin extends Client {
 				int b=articles.get(a); // on recupere l'int , le stock de l'article
 				articles.put(a, b-1); // diminue le stock de 1
 			}
-	} }
-	public void acheter(Fournisseur f, Article a, int nombreArticle) {
-		int n = (int) (nombreArticle*a.getPrixAchat()); // combien coute le total de ce que l'on achete
-		if ( n > this.revenu) {
-			System.out.println("Le magasin n a pas assez de tresorie pour acheter" + nombreArticle + a.getNom() +"au fournisseur" + f.getNomFourni());
-			return;
-		}
-		else {
-			this.revenu= this.revenu - n;
-			int b=articles.get(a);
-			articles.put(a,b+n);
-			int e = f.getStockFourni();
-			f.setStockFourni(e-n);
-		}
 	}
-		
-	
-	// Affiage partiel d'un article// 
+}	
+	// Verifier que tout s'affiche bien //
 	public static void afficherInventaire() {
 		 Iterator it = articles.entrySet().iterator();
-	        while (it.hasNext()) {
+	        while (it.hasNext()) {// has.Next, booleanqui indique s'il existe elements dans la collection cf cours 7
 	            Map.Entry m = (Map.Entry) it.next();
 	            System.out.println("Article: "+((Article) m.getKey()).getNom()+", Stock: "+m.getValue());
 	        }
 	}
+	public void supArticle(String s) {
+		Article a=traduction (s);
+		articles.remove(a);
+	}
 	
-	// Affiage complet d'un article // 
-	public static void afficherInventaireComplet() {
-		 Iterator it = articles.entrySet().iterator();
+	public static void afficherClient() { // Afficher la liste des clients 
+		 Iterator it = reduction.entrySet().iterator();
 	        while (it.hasNext()) {
 	            Map.Entry m = (Map.Entry) it.next();
-	            System.out.println("Article: "+((Article) m.getKey()).getNom()+" Description: "+((Article) m.getKey()).getDescription()+"  Prix d'achat: "+((Article) m.getKey()).getPrixAchat()+" Prix de vente: "+((Article) m.getKey()).getPrixVente()+" Promo ?: "+((Article) m.getKey()).getPromo() +" Stock 1: "+ ((Article) m.getKey()).getStock()+" Stock 2: "+m.getValue());
-	      
+	            System.out.println("Nom: "+((Client) m.getKey()).getNom()+"Prenom "+((Client) m.getKey()).getPrenom()+ "Nombre d'achat "+m.getValue());
 	        }
 	}
 	
-	
-	// Supprimer un article de la liste //
-	public static void supprimerArticle() {
-		FileDriver.afficherFichierSauvegarde(FileDriver.chemin ); 
-		System.out.println("Ecrivez le nom de l'article à supprimer");
-		System.out.println();
-		int id_article_supp = Magasin.objet.nextInt(); 
-		articles.remove(id_article_supp);
+	public void AfficherRevenu() {
+		System.out.println(this.revenu);
+	}
+	public Map<Client, Integer> getReduction() {
+		return this.reduction;
+	}
+	public double GetRevenu() {
+		return this.revenu;
 	}
 	
-	
-	
 }
+	
