5d461170aeea2e01e95b1cc85dbb2f91d204f014
Nath149 <81323071+Nath149@users.noreply.github.com>
2021-12-08 15:58:09 +0100

    Create Tuple.java

---
+++ Tuple.java
@@ -0,0 +1,19 @@
+public class Tuple<T1,T2> {
+	private T1 t1;
+	private T2 t2;
+	
+	public Tuple(T1 t1, T2 t2) {
+		this.setT1(t1);
+		this.t2=t2;
+	}
+	public T2 getT2() {
+		return this.t2;
+	}
+	public T1 getT1() {
+		return t1;
+	}
+	public void setT1(T1 t1) {
+		this.t1 = t1;
+	}
+	
+}
