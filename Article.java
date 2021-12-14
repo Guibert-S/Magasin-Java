18bdfc312aa0fcf479f8a7c21109b67182aa5829
Nath149 <81323071+Nath149@users.noreply.github.com>
2021-12-14 16:00:27 +0100

    Update Article.java

--- Article.java
+++ Article.java
@@ -1,22 +1,23 @@
 
-
 import java.util.Scanner;
 
 public class Article {
 	private String nom;
 	private double prixAchat;
 	private double prixVente;
+	private String categorie;// choix entre P:premium, C, confort et E, entré de gamme 
+	
 	
 
 
 // Constructeur //
 
 
-	public Article(String nom, double prixAchat, double prixVente) {
+	public Article(String nom, double prixAchat, double prixVente, String categorie ) {
 		this.nom =nom;
 		this.prixAchat =prixAchat ;
 		this.prixVente=prixVente;
-		
+		this.setCategorie(categorie);
 	}
 
 	public Article() {
@@ -30,7 +31,25 @@ public class Article {
 		System.out.print("Prix  de vente de l'article  :\n" );
 		prixVente = objet.nextDouble();
 		objet.nextLine() ; // si on met qu'une fois nextline ca marche pas pas donc bon ... //
+		
+		System.out.println("Categorie de l'article: P(premium), C(confort),E(entrée de gamme)");
+		categorie=objet.nextLine();
 			
+		
+	}
+	public Article(String nom) {
+		this.nom=nom;
+		
+		Scanner objet = new Scanner (System.in);
+		System.out.print("Prix d'achat de l'article :\n" );
+		prixAchat= objet.nextDouble();
+			
+		System.out.print("Prix  de vente de l'article  :\n" );
+		prixVente = objet.nextDouble();
+		objet.nextLine() ; // si on met qu'une fois nextline ca marche pas pas donc bon ... //
+		
+		System.out.println("Categorie de l'article: P(premium), C(confort),E(entrée de gamme)");
+		categorie=objet.nextLine();
 		
 	}
 	public String getNom() {
@@ -63,11 +82,24 @@ public class Article {
 	}
 
 	public void afficherArticle() {
-		System.out.println("Nom article: "+this.nom+"Prix d'achat: "+this.prixAchat+"Prix de vente: "+this.prixVente);
+		if(categorie=="P") {
+			System.out.println("Nom article: "+this.nom+" Prix d'achat: "+this.prixAchat+" Prix de vente: "+this.prixVente+" Categorie: Premium");
+		}
+		if(categorie=="C") {
+			System.out.println("Nom article: "+this.nom+" Prix d'achat: "+this.prixAchat+" Prix de vente: "+this.prixVente+" Categorie: Confort");
+		}
+		if(categorie=="E") {
+			System.out.println("Nom article: "+this.nom+" Prix d'achat: "+this.prixAchat+" Prix de vente: "+this.prixVente+" Categorie: Entree de gamme");
+		}
+	}
+
+	public String getCategorie() {
+		return categorie;
+	}
+
+	public void setCategorie(String categorie) {
+		this.categorie = categorie;
 	}
 	
 	
 }
-
-
-
