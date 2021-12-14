a94d881fb5635a00b7079ff39cbba35e3152be1d
Nath149 <81323071+Nath149@users.noreply.github.com>
2021-12-14 15:59:55 +0100

    Update Magasin.java

--- Magasin.java
+++ Magasin.java
@@ -2,62 +2,88 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Magasin {
+	private int jour;
 	private String nom;
 	private static double revenu;
-	private static double depense;
+	private static double charges;
 	// nombre d'articles dans le magasin //
-	private static Map<Article,Integer> articles = new HashMap<>();// dictionnaire reliant les articles avec leur stock 
-	private static Map <Client,Integer> reduction = new HashMap<>();// dictionnaire reliant le client avec leur nombre d'achats 
+	private static Map<Article,Integer> articles = new HashMap<Article,Integer>();// dictionnaire reliant les articles avec leur stock 
+	private static ArrayList <Client> clients = new ArrayList<Client>();// dictionnaire reliant le client avec leur nombre d'achats 
 	static Scanner objet = new Scanner (System.in);
-	private ArrayList <Employe> employes=new ArrayList <Employe>();// liste dynamique d'employe
-	public Magasin() {}
+	private ArrayList <Vendeur> employes=new ArrayList <Vendeur>();// liste dynamique de vendeur
+	
+	
+	public Magasin(String nom,double charges) {//initialisation du magasin avec les charges. 
+		this.jour=1;
+		this.setRevenu(2000);
+		this.nom=nom;
+		this.charges=charges;
+	}
 	
 	// ajout d'article et saisie controle avec la boucle while //
 	
-	
-	public Article traduction(String s) {
-		Iterator it=articles.entrySet().iterator();
+	public boolean contenu(String s,String categorie) {
+		Iterator it=getArticles().entrySet().iterator();
 		while(it.hasNext()) {
 			Map.Entry m=(Map.Entry) it.next();
-			if(((Article) m.getKey()).getNom()==s) {
+			if(((Article) m.getKey()).getNom()==s && ((Article) m.getKey()).getCategorie()==categorie) {
+				return true;
+			}
+		}
+		return false;
+	}
+		
+	
+	public Article traduction(String s,String categorie) {// on prend le nom d'un article et on renvoie le type Article correspondant dans l'inventaire
+		Iterator<Entry<Article, Integer>> it=getArticles().entrySet().iterator();
+		while(it.hasNext()) {
+			Map.Entry m=(Map.Entry) it.next();
+			if(((Article) m.getKey()).getNom()==s && ((Article) m.getKey()).getCategorie()==categorie) {
 				return (Article) m.getKey();
 			}
 		}
 		return null;
 		
-		
-		
 	}
-	public void ajoutEmploye(Employe e) {
+	public void ajoutEmploye(Vendeur e) {
 		employes.add(e);
 	}
 	public void afficherEmploye() {
-		Iterator<Employe> it =employes.iterator();
+		Iterator<Vendeur> it =employes.iterator();
 		while(it.hasNext()) {// tant que it.hasnext est different de nul, on continue dans la boucle.
-			Employe a=it.next();
+			Vendeur a=it.next();
 			System.out.println("Nom:"+a.getNom()+" Prenom: "+a.getPrenom());
 		}
 	}
-	public void licenciement(Employe e) {
-		employes.remove(e); // qaund on tombe sur l'employe à supprumer, on le supprime de l'array.		
-	}
 	// Ajouter l'article à la liste/array/dictionnaire //
 	
-	public static void ajouterArticle(Article a) {
-		System.out.println("Quelle est le stock de votre article ?");
+	public void ajouterArticle(Article a) { // on demande le stock au manager 
+		System.out.println("Quelle est le stock de votre article "+a.getNom()+"?");
 		int c=objet.nextInt();
-		articles.put(a, c);
+		getArticles().put(a, c);
 	}
 	
-	
+	public void AjouterArticlebis(Article a, int stock) {//on connait le stock en amont, si l'article n'existe pas, on l'ajoute
+		if( getArticles().containsKey(a)==true) {
+			int b=getArticles().get(a);
+			getArticles().replace(a,b,b+stock);
+		}
+		else {
+			getArticles().put(a,stock);
+		}
 		
 		
-	
-	public void vendre(Client c,Article a) {
-		int d=reduction.get(c);
+	}
+	public void vendre(Client c,Article a,Vendeur v) {
+		if(!(clients.contains(c))) {
+			clients.add(c);
+		}
+		
+		int d=c.getAchat();
 		if(c.getArgent()<a.getPrixVente()) {
 			System.out.println("Vous n'avez pas assez d'argent pour acheter cet article");
 			return;
@@ -65,49 +91,166 @@ public class Magasin {
 		else {
 			if(d>=5){// si le client a fait plus de 5 achats dans le magasin alors on applique une reduction de 20 %
 				c.setArgent(c.getArgent() - a.getPrixVente()*0.8);
-				revenu+=a.getPrixVente()*0.8;
-				reduction.put(c, d+1);
-				int b=articles.get(a); // on recupere l'int , le stock de l'article
-				articles.put(a, b-1); // diminue le stock de 1
+				setRevenu(getRevenu() + a.getPrixVente()*0.8);
+				int e=c.getAchat();
+				c.setAchat(e+1);
+				int b=getArticles().get(a); // on recupere l'int , le stock de l'article
+				getArticles().replace(a, b-1); // diminue le stock de 1
 			}
 			else {
 				c.setArgent(c.getArgent() - a.getPrixVente());
-				revenu+=a.getPrixVente();
-				reduction.put(c, d+1);
-				int b=articles.get(a); // on recupere l'int , le stock de l'article
-				articles.put(a, b-1); // diminue le stock de 1
+				setRevenu(getRevenu() + a.getPrixVente());
+				int e=c.getAchat();
+				c.setAchat(e+1);
+				int b=getArticles().get(a); // on recupere l'int , le stock de l'article
+				getArticles().replace(a, b-1); // diminue le stock de 1
 			}
 	}
-}	
+		int r=v.getVente();
+		v.setVente(r+1);
+}
+	public void filtre(String s){//filtre les produits en focntion de leur categorie, premium etc..
+		Iterator it = getArticles().entrySet().iterator();
+        while (it.hasNext()) {
+        	Map.Entry m=(Map.Entry) it.next();
+        	if(((Article) m.getKey()).getCategorie()==s) {
+        		((Article) m.getKey()).afficherArticle();
+        	}
+;        }
+	}
+	
+	public void dirigerClients(Client t) {// on va conseiller le client en fonction de son budget. 3 cas: premium, confort, entree de gamme 
+		if(t.getArgent()>1000) {
+			filtre("P");
+		}
+		if(t.getArgent()>500) {
+			filtre("C");
+		}
+		if(t.getArgent()<250) {
+			filtre("E");
+		}
+	}
+	
 	// Verifier que tout s'affiche bien //
-	public static void afficherInventaire() {
-		 Iterator it = articles.entrySet().iterator();
+	public void afficherInventaire() {
+		 Iterator it = getArticles().entrySet().iterator();
 	        while (it.hasNext()) {// has.Next, booleanqui indique s'il existe elements dans la collection cf cours 7
 	            Map.Entry m = (Map.Entry) it.next();
-	            System.out.println("Article: "+((Article) m.getKey()).getNom()+", Stock: "+m.getValue());
+	            System.out.println("Article: "+((Article) m.getKey()).getNom()+" Categorie:"+((Article) m.getKey()).getCategorie()+", Stock: "+m.getValue());
 	        }
 	}
-	public void supArticle(String s) {
-		Article a=traduction (s);
-		articles.remove(a);
+	public void supArticle(String s,String categorie) {
+		Article a=traduction (s,categorie);
+		getArticles().remove(a);
 	}
 	
 	public static void afficherClient() { // Afficher la liste des clients 
-		 Iterator it = reduction.entrySet().iterator();
+		 Iterator <Client>it = clients.iterator();
 	        while (it.hasNext()) {
-	            Map.Entry m = (Map.Entry) it.next();
-	            System.out.println("Nom: "+((Client) m.getKey()).getNom()+"Prenom "+((Client) m.getKey()).getPrenom()+ "Nombre d'achat "+m.getValue());
+	            Client m= it.next();
+	            System.out.println("Nom: "+ m.getNom()+"Prenom "+ m.getPrenom()+ "Nombre d'achat "+m.getAchat());
 	        }
 	}
 	
 	public void AfficherRevenu() {
-		System.out.println(this.revenu);
-	}
-	public Map<Client, Integer> getReduction() {
-		return this.reduction;
-	}
-	public double GetRevenu() {
-		return this.revenu;
+		System.out.println(this.getRevenu());
 	}
 	
-}	
+	public double GetRevenu() {
+		return this.getRevenu();
+	}
+	
+	public void finMois() {// permet de simuler le paiement des charges et le paiement des salaires (charges fixes)
+		if(jour==30) {
+			setRevenu(getRevenu() - charges);
+			for(Vendeur i:employes) {
+				setRevenu(getRevenu() - i.salaire);
+				i.setVente(0);
+				i.setSalaire(1800);
+			}
+			jour=1;
+		}
+		
+	}
+	public void acheter(Fournisseur t,String nom,String categorie,int quanti) {
+		Article a=this.traduction(nom,categorie);
+		revenu-=quanti*a.getPrixAchat();
+		a.afficherArticle();
+		int b=getArticles().get(a);
+		getArticles().replace(a,b,b+quanti);
+		t.diminuerDispo(a, quanti);
+		//penser à diminuer le stock
+	}
+	
+	public void acheterbis(Fournisseur t) {
+		System.out.println("Quels produits voulez vous acheter ?");
+		t.afficherProdFour();
+		System.out.println("Nom du produit:");
+		String nom=objet.nextLine();
+		System.out.println("1.Premium, 2.Confort,3.Entree de gamme");
+		int a=1;
+		String categorie="";
+		if(a==1) {
+			categorie="P";
+		}
+		if(a==2) {
+			categorie="C";
+		}
+		if(a==3) {
+			categorie="E";
+		}
+		System.out.println("Quantité: ");
+		int quanti=20;
+		System.out.println(nom+categorie+quanti);
+		acheter(t,nom,categorie,quanti);
+		
+		
+	}
+	
+	public static void main (String [] args ) {
+		Magasin mag=new Magasin("Decathlon",200);//200 correspond aux charges
+		Article a=new Article("Ski",200,400,"P");
+		Article b=new Article ("Ski",100,200,"C");
+		Article c=new Article ("Ski",60,120,"E");
+		mag.AjouterArticlebis(a,1);
+		mag.AjouterArticlebis(b,1);
+		mag.AjouterArticlebis(c,1);
+		Vendeur jo=new Vendeur("john","doe",Poste.Vendeur,0);
+		jo.affichage();
+		mag.ajoutEmploye(jo);
+		Client bob=new Client ("bob","bdd",1000,0,"");
+		mag.vendre(bob, a, jo);
+		jo.affichage();
+		ArrayList <Article> a1=new ArrayList<Article>();
+		a1.add(a);
+		a1.add(b);
+		a1.add(c);
+		
+				
+		
+		
+		
+	}
+
+	public static Map<Article,Integer> getArticles() {
+		return articles;
+	}
+
+	
+	public static void setArticles(Map<Article,Integer> articles) {
+		Magasin.articles = articles;
+	}
+
+	public static double getRevenu() {
+		return revenu;
+	}
+
+	public static void setRevenu(double revenu) {
+		Magasin.revenu = revenu;
+	}
+
+}
+
+	
+
+
