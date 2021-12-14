0df56e8f911732c7ec933f44ead5007435e4b599
ameliemelo <90570936+ameliemelo@users.noreply.github.com>
2021-12-14 22:07:11 +0100

    Update Vendeur.java

--- Vendeur.java
+++ Vendeur.java
@@ -1,3 +1,5 @@
+/** classe pour creer un vendeur qui est un employé */
+
 public class Vendeur extends Employe{
 	private int nmbreVente;
 	
@@ -6,24 +8,39 @@ public class Vendeur extends Employe{
 		this.salaire=1800;
 		this.nmbreVente=vente;
 	}
+	
+	/** methode pour avoir une augmentation de salaire si le nombre de vente du vendeur est superieur a 50
+	 */
 	public void recevoirSalaire(){
 		if(this.nmbreVente>50) {
-			this.setSalaire(1800+200);// bonus pour le nombre de vente
+			this.setSalaire(1800+200);
 		}
 	}
 	
+	/** methode pour afficher le nom, le prenom, le poste, le salaire et le nombre de vente du vendeur
+	 */
 	public void affichage () {
 		String str =String.valueOf(this.salaire);
 		String str2=String.valueOf(this.nmbreVente);
 		System.out.println("Nom: " +this.nom+"\nPrenom: "+this.prenom+"\nPoste: "+emploi.toString()+"\nBourse: "+str+"\nNombre de Ventes: "+str2);
 	}
+	
+	/** getter pour donner le nombre de ventes du vendeur 
+	 */
 	public int getVente() {
 		return nmbreVente;
 	}
+	
+	/** setter pour recuperer le nombre de ventes du vendeur 
+	 * @param le nombre de ventes
+	 */
 	public void setVente(int x) {
 		this.nmbreVente=x;
 	}
 	
+	/** setter pour recuperer le salaire du vendeur 
+	 * @param le salaire 
+	 */
 	public void setSalaire(int x) {
 		this.salaire=x;
 	}
