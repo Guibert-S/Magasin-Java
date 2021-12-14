7ffcad42d6967cb73d37b284920a42945e464834
Nath149 <81323071+Nath149@users.noreply.github.com>
2021-12-14 15:53:17 +0100

    Update Fournisseur.java

--- Fournisseur.java
+++ Fournisseur.java
@@ -14,7 +14,7 @@ public class Fournisseur {// créer differents types de fourniseurs: articles de
 		return this.nomFourni;
 	}
 	
-	public Fournisseur () {
+	public Fournisseur (Magasin m) {
 		System.out.println("Nom du fournisseur ? ");
 		Scanner objet2=new Scanner (System.in);
 		String g=objet2.nextLine();
@@ -22,15 +22,32 @@ public class Fournisseur {// créer differents types de fourniseurs: articles de
 		System.out.println("Combien d'articles produisez vous ?");
 		Scanner objet = new Scanner (System.in);
 		int c=objet.nextInt();
+		Scanner yu =new Scanner(System.in);
 		int i=0;
 		while(i<c) {
-			Article a=new Article();
+			System.out.println("Nom du produit: ");
+			String nom=yu.nextLine();
+			Article b=new Article(nom);
 			System.out.println("Combien en avez vous ?");
 			int d=objet.nextInt();
-			Tuple <Article,Integer>t=new Tuple<Article, Integer>(a,d);
-			artfourni.add(t);
+			Tuple <Article,Integer>t3=new Tuple<Article, Integer>(b,d);
+			artfourni.add(t3);
+			
 			i+=1;
+			
 		}
+	}
+	public Fournisseur(String nom, ArrayList<Article> article,int stock) {//article et stock de meme taille 
+		this.nomFourni=nom;
+		Iterator <Article> it=article.iterator();
+		
+		while(it.hasNext()) {
+			Article a=it.next();
+			
+			Tuple<Article,Integer> t=new Tuple <Article,Integer> (a,stock);
+			artfourni.add(t);
+		}
+		
 	}
 	public void afficherProdFour() {
 		System.out.println("Nom du fournisseur: "+nomFourni);
@@ -46,11 +63,33 @@ public class Fournisseur {// créer differents types de fourniseurs: articles de
 		
 		
 	}
+	
+	public void diminuerDispo(Article a,int x) {
+		Iterator<Tuple<Article,Integer>> it=artfourni.iterator();
+		while(it.hasNext()) {
+			Tuple<Article,Integer> t=it.next();
+			if(t.getT1().getNom()==a.getNom()&& t.getT1().getCategorie()==a.getCategorie()) {
+				t.setT2(t.getT2()-x);
+			}
+		}
+
+		
+	}
+	public ArrayList<Tuple<Article, Integer>> getTuple() {
+		return artfourni;
+	}
 	public static void main(String[] args) {
-		Fournisseur f=new Fournisseur();
-		Fournisseur t=new Fournisseur();
+		ArrayList <Article> a1=new ArrayList<Article>();
+		Article a=new Article("Ski",200,400,"P");
+		Article b=new Article ("Ski",100,200,"C");
+		Article c=new Article ("Ski",60,120,"E");
+		a1.add(a);
+		a1.add(b);
+		a1.add(c);
+		Fournisseur f=new Fournisseur("defe",a1,100);
+		f.diminuerDispo(a, 20);
 		f.afficherProdFour();
-		t.afficherProdFour();
+		
 		
 	}
 }
