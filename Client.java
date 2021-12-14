04d59d583c3d8967aa33aaf1fa5e48291bd1c61e
ameliemelo <90570936+ameliemelo@users.noreply.github.com>
2021-12-14 19:43:50 +0100

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,3 +1,4 @@
+/** classe pour creer un client */
 public abstract class Client {
 	private String nom;
 	private String prenom;
@@ -8,15 +9,31 @@ public abstract class Client {
 		this.prenom=prenom;
 		this.setArgent(argent);
 	}
+	
+	/**getter pour donner l'argent que possede le client
+	 * @return double l'argent que possede le client
+	 */
 	public double getArgent() {
 		return this.argent;
 	}
+	
+	/**getter pour donner le nom du client
+	 * @return String le nom du client
+	 */
 	public String getNom() {
 		return this.nom;
 	}
+
+	/**getter pour donner le prenom du client
+	 * @return String le prenom du client
+	 */
 	public String getPrenom() {
 		return this.prenom;
 	}
+	
+	/**setter pour recuperer l'argent que possede le client
+	 * @param l'argent que possede le client en question
+	 */
 	public void setArgent(double argent) {
 		this.argent = argent;
 	}
