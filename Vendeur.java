2442094f46e9dfc2e90f704e6c44785b89ce97d4
Nath149 <81323071+Nath149@users.noreply.github.com>
2021-12-14 15:52:35 +0100

    Update Vendeur.java

--- Vendeur.java
+++ Vendeur.java
@@ -1,24 +1,31 @@
 public class Vendeur extends Employe{
 	private int nmbreVente;
 	
-	public Vendeur(String nom,String prenom,Poste emploi,int salaire,int vente) {
-		super(nom,prenom,emploi,salaire);
+	public Vendeur(String nom,String prenom,Poste emploi,int vente) {
+		super(nom,prenom,emploi);
+		this.salaire=1800;
 		this.nmbreVente=vente;
 	}
 	public void recevoirSalaire(){
-		this.salaire+=100;  // Salaire mensuel 
-		if(this.nmbreVente>10) {
-			this.salaire+=20;// bonus pour le nombre de vente
+		if(this.nmbreVente>50) {
+			this.setSalaire(1800+200);// bonus pour le nombre de vente
 		}
-		
-		
 	}
+	
 	public void affichage () {
 		String str =String.valueOf(this.salaire);
 		String str2=String.valueOf(this.nmbreVente);
-		String str3=String.valueOf(this.emploi);
-		System.out.println("Nom: " +this.nom+"Prenom: "+this.prenom+"Nombre de Ventes: "+"Poste: "+str3+"Bourse: "+str+"Nombre de Ventes: "+str2);
+		System.out.println("Nom: " +this.nom+"\nPrenom: "+this.prenom+"\nPoste: "+emploi.toString()+"\nBourse: "+str+"\nNombre de Ventes: "+str2);
+	}
+	public int getVente() {
+		return nmbreVente;
+	}
+	public void setVente(int x) {
+		this.nmbreVente=x;
 	}
 	
+	public void setSalaire(int x) {
+		this.salaire=x;
+	}
 	
 }
