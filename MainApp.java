84efe395304b6f5a91488852d01b36463cb83fae
Nath149 <81323071+Nath149@users.noreply.github.com>
2021-12-19 15:04:32 +0100

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -12,6 +12,14 @@ public class MainApp {
 		mag.AjouterArticlebis(a,1);
 		mag.AjouterArticlebis(b,2);
 		mag.AjouterArticlebis(c,3);
+		ArrayList <Article> a1=new ArrayList<Article>();
+		Article a=new Article("Ski",200,400,"P");
+		Article b=new Article ("Ski",100,200,"C");
+		Article c=new Article ("Ski",60,120,"E");
+		a1.add(a);
+		a1.add(b);
+		a1.add(c);
+		Fournisseur f=new Fournisseur("Fournisseur1",a1,100);
 		FileDriver.ecrireFichier(FileDriver.chemin,Magasin.getArticles());
 		
 		FileDriver.lireFichierSauvegarder(FileDriver.chemin);
